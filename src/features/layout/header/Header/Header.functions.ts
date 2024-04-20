2ee3e566f345b0734c2df577291fdf8efdebd081
Johan Petrikovsky <johan.petrikovsky@gmail.com>
2024-04-20 14:59:39 +0200

    feqat: update card

--- src/features/layout/header/Header/Header.functions.ts
+++ src/features/layout/header/Header/Header.functions.ts
@@ -1,8 +1,25 @@
-export async function updateBitcoinPrice(): Promise<string> {
-  const response = await fetch(
-    "https://api.coinbase.com/v2/prices/spot?currency=USD",
-  );
-  const data = await response.json();
+export async function updateBitcoinPrice(): Promise<string | null> {
+  try {
+    const response = await fetch(
+      "https://api.coinbase.com/v2/prices/spot?currency=USD",
+    );
 
-  return data?.data?.amount;
+    if (!response.ok) {
+      console.error(`Error fetching Bitcoin price: ${response.statusText}`);
+      return null;
+    }
+
+    const data = await response.json();
+    const price = data?.data?.amount;
+
+    if (price === undefined) {
+      console.error("Bitcoin price not found in response");
+      return null;
+    }
+
+    return price;
+  } catch (error) {
+    console.error(`Error fetching Bitcoin price: ${error}`);
+    return null;
+  }
 }
