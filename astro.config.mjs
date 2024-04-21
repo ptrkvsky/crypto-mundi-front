070fb197f64ecf1a9690163d7d588fc878d0aa97
Johan Petrikovsky <johan.petrikovsky@gmail.com>
2024-04-21 11:06:54 +0200

    feat: :sparkles: add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,23 @@
-import { defineConfig } from 'astro/config';
-
+import { defineConfig } from "astro/config";
 import sanity from "@sanity/astro";
 import react from "@astrojs/react";
 
+import sitemap from "@astrojs/sitemap";
+import { getCurrentDate } from "./src/helpers/getCurrentDate";
+// get current date based this format '2023-03-20'
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [sanity({
-    projectId: 'g8dp9xkw',
-    dataset: 'production',
-    useCdn: false, // See note on using the CDN
-    apiVersion: "2023-03-20", // insert the current date to access the latest version of the API
-  }), react()]
+  site: "https://www.crypto-mundi.com",
+  integrations: [
+    sanity({
+      projectId: "g8dp9xkw",
+      dataset: "production",
+      useCdn: false,
+      // See note on using the CDN
+      apiVersion: getCurrentDate(), // insert the current date to access the latest version of the API
+    }),
+    react(),
+    sitemap(),
+  ],
 });
