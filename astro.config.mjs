cc4f01dd00bd49c6a53554c32126638ce0317263
Johan Petrikovsky <johan.petrikovsky@gmail.com>
2024-04-29 22:32:17 +0200

    feat: add pwa

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from "astro/config";
 import sanity from "@sanity/astro";
 import react from "@astrojs/react";
-import AstroPWA from "@vite-pwa/astro";
+import { VitePWA } from "vite-plugin-pwa";
 import { getCurrentDate } from "./src/helpers/getCurrentDate";
+import { manifest } from "./src/config";
 // get current date based this format '2023-03-20'
 
 // https://astro.build/config
@@ -17,11 +18,28 @@ export default defineConfig({
       apiVersion: getCurrentDate(), // insert the current date to access the latest version of the API
     }),
     react(),
-    AstroPWA(),
   ],
   redirects: {
     "/article/Link/": "/",
   },
   base: "/",
   trailingSlash: "never",
+  vite: {
+    plugins: [
+      VitePWA({
+        registerType: "autoUpdate",
+        manifest,
+        strategies: "generateSW",
+        workbox: {
+          globDirectory: "dist",
+          globPatterns: [
+            "**/*.{js,css,svg,png,jpg,jpeg,gif,webp,woff,woff2,ttf,eot,ico}",
+          ],
+          // Don't fallback on document based (e.g. `/some-page`) requests
+          // Even though this says `null` by default, I had to set this specifically to `null` to make it work
+          navigateFallback: null,
+        },
+      }),
+    ],
+  },
 });
--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,5 @@
+import type { ManifestOptions } from "vite-plugin-pwa";
+
 const projectId = import.meta.env.PUBLIC_SANITY_PROJECT_ID;
 const version = import.meta.env.PUBLIC_SANITY_API_VERSION;
 const dataSet = import.meta.env.PUBLIC_SANITY_DATASET;
@@ -7,6 +9,37 @@ const config = {
   mode: dataSet,
   projectId,
   sanityGraphqlEndpoint: `https://${projectId}.api.sanity.io/${version}/graphql/${dataSet}/default`,
+};
+
+/**
+ * Defines the configuration for PWA webmanifest.
+ */
+export const manifest: Partial<ManifestOptions> = {
+  name: "Johan Petrikovsky développeur React Freelance", // Change this to your website's name.
+  short_name: "Site de Johan Petrikovsky", // Change this to your website's short name.
+  description:
+    "Johan Petrikovsky est un développeur React et Typescript Freelance.", // Change this to your websites description.
+  theme_color: "#30E130", // Change this to your primary color.
+  background_color: "#ffffff", // Change this to your background color.
+  display: "minimal-ui",
+  icons: [
+    {
+      src: "/favicons/android-chrome-192x192.webp",
+      sizes: "192x192",
+      type: "image/webp",
+    },
+    {
+      src: "/favicons/android-chrome-512x512.webp",
+      sizes: "512x512",
+      type: "image/webp",
+    },
+    {
+      src: "/favicons/android-chrome-512x512.webp",
+      sizes: "512x512",
+      type: "image/webp",
+      purpose: "any maskable",
+    },
+  ],
 };
 
 export default config;
