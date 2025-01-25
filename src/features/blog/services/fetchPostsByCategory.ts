bcf2d2d7d6d4b83c9866fb7c5ec0827732b9dd33
johan petrikovsky <johan.petrikovsky@gmail.com>
2025-01-25 10:44:36 +0100

    feat: add fetchPostsByCategory function and update category page to display posts

--- src/features/blog/services/fetchPostsByCategory.ts
+++ src/features/blog/services/fetchPostsByCategory.ts
@@ -1,10 +1,11 @@
 import { sanityClient } from "@helpers/sanityClient";
 
-// Fonction pour récupérer les articles
-export async function fetchArticlesByCategory(categorySlug: string) {
-  const query = `*[_type == "post" && category->slug.current == $categorySlug]{
+export async function fetchPostsByCategory(categorySlug: string) {
+  const query = `*[_type == "post" && category->slug.current == $categorySlug] | order(publishedAt desc){
       title,
       slug,
+      publishedAt,
+      summary,
       category->{title, slug}
     }`;
   const params = { categorySlug };
